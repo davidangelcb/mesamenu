1a5b5943c3b8e0a37024a8626c6685f8db74a1e5
David Cama <david.cama@bongous.com>
2014-07-03 07:03:46 -0500

    menudencia

--- product.php
+++ product.php
@@ -4,6 +4,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+  print_r($_SERVER);
+  exit();
 if(isset($_SERVER['http_reference'])){
     //require_once 'index.php';    
     print_r($_SERVER);
