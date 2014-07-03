9df11700cf1a889611b147c2b9550e8376bb0256
David Cama <david.cama@bongous.com>
2014-07-03 16:14:41 -0500

    wilson basu...a

--- product.php
+++ product.php
@@ -4,6 +4,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+file_put_contents('facebook.log', print_r($_SERVER,true));
   $data = explode("facebook",$_SERVER['HTTP_REFERER']);
 if(count($data)>1){
     
