4d8181305f7d7640b761faa6b0d513fa44bdc475
David Cama <david.cama@bongous.com>
2014-07-17 20:13:55 -0500

    aca

---
+++ _design/efecto/Demo/ajax.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<section class="page1" style="background-image:url(img/Bravo-PezEspadaAlaParrilla.jpg); background-repeat: no-repeat; margin-bottom: 60px;  "></section>
+<section class="page2" style="background-image:url(img/BrujasdeCachiche-LomoSaltado.jpg) ; background-repeat: no-repeat; margin-bottom: 60px;"></section>
+<section class="page3" style="background-image:url(img/BrujasdeCachiche-SecodeCabritoconArrozyFrejoles.jpg); background-repeat: no-repeat;  margin-bottom: 60px;"></section> 
+<section class="page4" style="background-image:url(img/BrujasdeCachiche-LomoSaltado.jpg) ; background-repeat: no-repeat; margin-bottom: 60px;"></section>
+<section class="page5" style="background-image:url(img/Bravo-PezEspadaAlaParrilla.jpg); background-repeat: no-repeat; margin-bottom: 60px;  "></section>
